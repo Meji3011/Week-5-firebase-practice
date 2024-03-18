59a7120c650330a15a26cca95c27a9f73e73e066
Ryan <tangguangran@gmail.com>
2024-03-18 16:44:49 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { auth } from "./firebase/init";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+
+function App() {
+  const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setLoading(false);
+      console.log(user);
+      if (user) {
+        setUser(user);
+      }
+    });
+  }, []);
+
+  function register() {
+    console.log("register");
+    createUserWithEmailAndPassword(auth, "email@email.com", "test123")
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  function login() {
+    signInWithEmailAndPassword(auth, "email@email.com", "test123")
+      .then(({ user }) => {
+        console.log(user);
+        setUser(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  function logout() {
+    signOut(auth);
+    setUser({});
+  }
+  return (
+    <div className="App">
+      <button onClick={register}>Register</button>
+      <button onClick={login}>Login</button>
+      <button onClick={logout}>Logout</button>
+      {loading ? 'Loading...' : user.email}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase/init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBdgQYnzlPKOOCeW9_az2dpDpcN6OayYuE",
+  authDomain: "fir-practice-323ff.firebaseapp.com",
+  projectId: "fir-practice-323ff",
+  storageBucket: "fir-practice-323ff.appspot.com",
+  messagingSenderId: "1065057119303",
+  appId: "1:1065057119303:web:c1180c14cd87ef29c59562"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
